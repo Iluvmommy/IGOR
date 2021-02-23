62e7c12e01644c62e61d6be9b47efe99ae955e0c
Iluvmommy <43627632+Iluvmommy@users.noreply.github.com>
2021-02-22 19:43:26 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const chalk = require("chalk");
+const axios = require("axios");
+const AV = require("av");
+require("aac");
+const pcm = require('pcm-util')
+
+app.use(
+  cors({
+    origin: ["http://itunes.apple.com/", "https://itunes.apple.com/"],
+  })
+);
+
+app.set("view-engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
+
+app.get("/results", async function (req, res) {
+  const query = req.query.search || "John Lennon";
+  console.log("Searching for " + query + "...");
+
+  await axios
+    .get("https://itunes.apple.com/search?term=" + query)
+    .then((res) => res.data)
+    .then((json) => {
+      var arr = [];
+      const item = json.results;
+      for (var i = 0; i < 10; i++) {
+        arr.push({
+          Artist: item[i].artistName,
+          Collection: item[i].collectionName,
+          Track: item[i].trackName,
+          Preview: item[i].previewUrl,
+          Id: item[i].trackId,
+        });
+      }
+      return arr;
+    })
+    .then((arr) => res.render("results.ejs", { res: res, data: arr }))
+    .catch((e) => console.log(e));
+});
+
+app.get("/sending", async (req, res) => {
+  try {
+    var url = req.query.url.replace("https", "http");
+    var asset = AV.Asset.fromURL(url);
+
+    console.log(chalk.cyan("Converting to buffer..."));
+    await asset.decodeToBuffer(function (buffer) {
+
+      console.log(chalk.green("Done Converting"));
+
+      
+      return res.render("sent.ejs", {buffer: buffer.slice(0, 10)});
+    });
+  } catch (e) {
+    return res.render("error.ejs", { error: e });
+  }
+});
+
+app.listen(3000, () => {
+  console.log(chalk.blue("Server Ready!"));
+});
